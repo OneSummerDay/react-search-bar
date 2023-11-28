ecbc589576e63fbf51ecba5db0dd6472a8df0e58
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-11-28 08:42:31 +0200

    feat: add searching filter logic

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,10 +4,18 @@ import SearchIcon from '@mui/icons-material/Search'
 
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([])
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value
+    const newFilter = data.filter((value) => {
+      return value.title.includes(searchWord)
+    })
+    setFilteredData(newFilter)
+  }
   return (
     <div className="search">
       <div className="searchInput">
-        <input type="text" placeholder={placeholder} />
+        <input type="text" placeholder={placeholder} onChange={handleFilter} />
         <div className="searchIcon">
           <SearchIcon />
         </div>

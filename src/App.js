7743e64f6c6f2d7215e6087c50287a36d43c14a5
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-11-28 08:03:28 +0200

    style: add SearchBar styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import SearchBar from './Components/SearchBar'
+import BookData from './Data.json'
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar placeholder="Enter a Book Name" data={BookData} />
     </div>
   )
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './SearchBar.css'
 
 function SearchBar({ placeholder, data }) {
   return (
     <div className="search">
       <div className="searchInput">
-        <input type="text" />
+        <input type="text" placeholder={placeholder} />
         <div className="seacrhIcon"></div>
       </div>
       <div className="dataResult"></div>

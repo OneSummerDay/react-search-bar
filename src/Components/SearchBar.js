844e3eaa6383bf088eb863e9e2dd1499953aee3c
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-11-28 09:04:44 +0200

    feat: add search removing logic

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import './SearchBar.css'
 import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close'
 
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([])
+  const [wordEntered, setWordEntered] = useState('')
 
   const handleFilter = (event) => {
     const searchWord = event.target.value
+    setWordEntered(searchWord)
     const newFilter = data.filter((value) => {
       return value.title.toLowerCase().includes(searchWord.toLowerCase())
     })
@@ -17,12 +20,26 @@ function SearchBar({ placeholder, data }) {
       setFilteredData(newFilter)
     }
   }
+
+  const clearInput = () => {
+    setFilteredData([])
+    setWordEntered('')
+  }
   return (
     <div className="search">
       <div className="searchInput">
-        <input type="text" placeholder={placeholder} onChange={handleFilter} />
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
         <div className="searchIcon">
-          <SearchIcon />
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
         </div>
       </div>
       {filteredData.length != 0 && (
